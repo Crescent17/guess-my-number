d25b7cc74429690fcabcfcf43afe0cc2c0865336
Marat <94042173+Crescent17@users.noreply.github.com>
2023-07-22 17:00:30 +0300

    add variable for highscore and handle button click

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 'use strict';
 
-const number = Math.trunc(Math.random() * 20) + 1;
+let number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
+
+const displayMessage = function(message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    displayMessage('No number!');
+  } else if (guess === number) {
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    displayMessage('Congratulations!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = number;
+  } else if (guess !== number) {
+    score--;
+    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      displayMessage(guess > number ? 'Too high!' : 'Too low!');
+    } else {
+      displayMessage('You\'ve lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
