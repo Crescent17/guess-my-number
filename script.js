b6e266154d938a029364b89574245b55dc179f1e
Marat <94042173+Crescent17@users.noreply.github.com>
2023-07-22 17:00:54 +0300

    add functionality for refreshing game

--- script.js
+++ script.js
@@ -32,3 +32,14 @@ document.querySelector('.check').addEventListener('click', () => {
     }
   }
 });
+
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  number = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
